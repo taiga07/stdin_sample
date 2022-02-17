a1f84702d9dff152e207b2f9d7beba9f3ea747e8
taiga07 <taigatani@me.com>
2022-02-17 12:19:35 +0000

    [アルゴリズム]

---
+++ gets_practice.rb
@@ -0,0 +1,7 @@
+# line = gets.split(' ')
+# p line
+
+line = gets.split(' ')
+line.each do |line|
+  p line
+end
---
+++ readlines_practice.rb
@@ -0,0 +1,12 @@
+# lines = readlines
+# lines.each do |words|
+#   p words.chomp.split(' ')
+# end
+
+lines = readlines
+lines.each do |words|
+  words.chomp.split(' ').each do |word|
+    p word
+  end
+end
+
